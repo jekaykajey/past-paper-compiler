6e1228ff88ff5f9af3216ffba8dba8ad5c447633
jekaykajey <heyitsmuadz@gmail.com>
2026-09-17 08:10:41 +0700

    Added key to .env file for database connection and fixed indentation in db.py for psycopg2.connect.

--- database/db.py
+++ database/db.py
@@ -9,6 +9,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 def init_db():
     # 1. Connect to PostgreSQL using psycopg2.connect(DATABASE_URL)
+    conn = psycopg2.connect(DATABASE_URL)
     # 2. Open 'database/schema.sql' and read its contents
     # 3. Use cursor.execute() to run the SQL commands
     # 4. Commit changes and close the connection
